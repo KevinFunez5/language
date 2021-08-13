4e3390a83b12c95c963c75199ceeb907e37d5e32
Kevin Funez <kevin.funez315@gmail.com>
2021-08-13 16:31:24 -0700

    rest of solutions for questions

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,19 @@ $(document).ready(function() {
     const expirience = parseInt($("input#inputExpirience"))
 
     if (age >= 80 && zip >= 90000 && sport === 1 &&  subject === 1 ){
-      $("span#result").text("Javascript")
+      $("span#result").text("Swift")
     } else if (age < 60 && zip >= 70000 && sport === 2 &&  subject === 2 ){
       $("span#result").text("C#")
     } else if (age < 50 && zip >= 60000 && sport === 3 &&  subject === 3 ){
-      $("span#result").text("C#")
-    } else if (age < 40 && zip >= 50000 && sport === 2 &&  subject === 2 ){
-      $("span#result").text("C#")
-    } else if (age < 30 && zip >= 40000 && sport === 2 &&  subject === 2 ){
-      $("span#result").text("C#")
-    } else if (age < 20 && zip >= 30000 && sport === 2 &&  subject === 2 ){
-      $("span#result").text("C#")
-    } else if (age < 18 && zip >= 20000 && sport === 2 &&  subject === 2 ){
-      $("span#result").text("C#")
+      $("span#result").text("Ruby")
+    } else if (age < 40 && zip >= 50000 && sport === 4 &&  subject === 4 ){
+      $("span#result").text("Go")
+    } else if (age < 30 && zip >= 40000 ){
+      $("span#result").text("Python")
+    } else if (age < 20 && zip >= 30000 ){
+      $("span#result").text("Rust")
+    } else {
+      $("span#result").text("Javascript")
     }
   });
 });
