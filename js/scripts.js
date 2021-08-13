d390dfe1e292f729a3ab4cb305dd3d01ba9029cb
Kevin Funez <kevin.funez315@gmail.com>
2021-08-13 13:35:51 -0700

    subject and expirience question

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
     const zip = parseInt($("input#inputZip").val());
     const sport = parseInt($("input#inputSport").val());
 
-    if ((age + zip) && () >= 100){
-      $("span#result").text("Works")
+    if ((age + zip) >= 90000){
+      $("span#result").text("Javascript")
+    } else if ((age + zip) > 80000) {
+      $("span#result").text("Python")
+    } else if ((age + zip) >= 40000) {
+      $("span#result").text("C++")
+    } else if ((age + zip) < 40000) {
+      $("span#result").text("Ruby")
     }
   })
 })
