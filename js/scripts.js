95ddc7c0a3592b56b17d0fe01683c4ffa038b479
Kevin Funez <kevin.funez315@gmail.com>
2021-08-13 15:42:33 -0700

    answers to questions

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,24 @@ $(document).ready(function() {
     event.preventDefault();
     const age = parseInt($("input#inputAge").val());
     const zip = parseInt($("input#inputZip").val());
-    const sport = parseInt($("input#inputSport").val());
-    const subject = parseInt($("input#input").val());
+    const sport = parseInt($("#inputSport").val());
+    const subject = parseInt($("#inputSubject").val());
+    const expirience = parseInt($("input#inputExpirience"))
 
-    if ((age + zip) >= 90000){
+    if (age >= 80 && zip >= 90000 && sport === 1 &&  subject === 1 ){
       $("span#result").text("Javascript")
-    } else if ((age + zip) > 80000) {
-      $("span#result").text("Python")
-    } else if ((age + zip) >= 40000) {
-      $("span#result").text("C++")
-    } else if ((age + zip) < 40000) {
-      $("span#result").text("Ruby")
+    } else if (age < 60 && zip >= 70000 && sport === 2 &&  subject === 2 ){
+      $("span#result").text("C#")
+    } else if (age < 50 && zip >= 60000 && sport === 3 &&  subject === 3 ){
+      $("span#result").text("C#")
+    } else if (age < 40 && zip >= 50000 && sport === 2 &&  subject === 2 ){
+      $("span#result").text("C#")
+    } else if (age < 30 && zip >= 40000 && sport === 2 &&  subject === 2 ){
+      $("span#result").text("C#")
+    } else if (age < 20 && zip >= 30000 && sport === 2 &&  subject === 2 ){
+      $("span#result").text("C#")
+    } else if (age < 18 && zip >= 20000 && sport === 2 &&  subject === 2 ){
+      $("span#result").text("C#")
     }
-  })
-})
+  });
+});
